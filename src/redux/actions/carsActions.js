5e0389c2222669239f5dfd65f9b2c3be6d451fb8
Ratheesh R <145856381+Vignesh12111@users.noreply.github.com>
2024-12-09 01:14:33 +0530

    fix the bug

--- src/redux/actions/carsActions.js
+++ src/redux/actions/carsActions.js
@@ -6,7 +6,7 @@ export const getAllCars=()=>async dispatch=>{
     dispatch({type: 'LOADING' , payload:true})
 
     try {
-        const response = await axios.get('/api/cars/getallcars')
+        const response = await axios.get('https://marketplace-for-rental-equipment-backend.onrender.com/api/cars/getallcars')
         dispatch({type: 'GET_ALL_CARS', payload:response.data})
         dispatch({type: 'LOADING' , payload:false})
     } catch (error) {
